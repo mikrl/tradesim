e4013a0c9b5df58c05911b73747bcc974858e6e3
Michael <mikrlynch@gmail.com>
2024-08-19 22:22:27 -0400

    Add missing header file function def

--- src/output.h
+++ src/output.h
@@ -7,6 +7,7 @@
 #include "trading_day.h"
 
 void const print(std::unordered_map<std::string, std::vector<OHLCData>> trading_day_data);
+std::stringstream  _construct_gnuplot_datafile(std::vector<OHLCData> trading_day_data);
 std::vector<std::stringstream> const format_as_gnuplot(std::unordered_map<std::string, std::vector<OHLCData>> trading_day_data);
 
 void const save(std::stringstream &file, std::string filename);
